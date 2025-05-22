5b13b2d8d16af1548ddb7cbab55f217d1db912ae
Vansh Maheshwari <121272010+Vansh98789@users.noreply.github.com>
2025-05-22 13:12:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,10 @@ import axios from "axios";
 const app = express();
 const port = 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://prep-sphere-frontend.vercel.app/'],
+  credentials: true
+}));
 app.use(express.json());
 
 // google api key
